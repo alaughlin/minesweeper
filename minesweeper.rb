b7e423d38bc7b6c18f356ca9d6ee4142237b520b
App Academy Student <student@appacademy.io>
2014-07-14 11:04:59 -0400

    Add display method to Board class.

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,16 +1,41 @@
 class Tile
-  attr_accessor :flagged, :revealed
-  attr_reader :bomb
-
   def initialize(bomb = false)
     @bomb = bomb
     @flagged = false
     @revealed = false
   end
+
+  def bomb?
+    @bomb
+  end
+
+  def flagged=(val)
+    @flagged = val
+  end
+
+  def flagged?
+    @flagged
+  end
+
+  def revealed=(val)
+    @revealed = val
+  end
+
+  def revealed?
+    @revealed
+  end
 end
 
 class Board
   def initialize
-    tiles = Array.new(9) { Array.new(9) { Tile.new(rand(5) > 1 ? false : true) } }
+    @tiles = Array.new(9) { Array.new(9) { Tile.new(rand(5) > 1 ? false : true) } }
+  end
+
+  def display
+    @tiles.each do |row|
+      row.each do |tile|
+        p tile.revealed?
+      end
+    end
   end
 end
